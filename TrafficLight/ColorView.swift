6cc99e2c7070ec0bab9a87367070e2a2b6ea4d49
LapKiSer <lapkovskiykirill@gmail.com>
2023-06-08 12:41:17 +0300

    Add ColorView

---
+++ TrafficLight/ColorView.swift
@@ -0,0 +1,20 @@
+//
+//  ColorView.swift
+//  TrafficLight
+//
+//  Created by Кирилл Лапковский on 08.06.2023.
+//
+
+import SwiftUI
+
+struct ColorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorView()
+    }
+}
--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -9,11 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        Color(.black)
+            .ignoresSafeArea()
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
         }
         .padding()
     }
